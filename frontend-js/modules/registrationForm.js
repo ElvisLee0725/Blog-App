dc48e710b3ae41210223376cb2139eaf268ebe07
ElvisLee <elvislee0725@gmail.com>
2020-08-18 17:53:40 -0700

    Make password at least 6 in length

--- frontend-js/modules/registrationForm.js
+++ frontend-js/modules/registrationForm.js
@@ -1,196 +1,229 @@
 import axios from 'axios';
 
 export default class RegistrationForm {
-    constructor() {
-        this.formSubmitHandler = this.formSubmitHandler.bind(this);
-        this._csrf = document.querySelector('[name="_csrf"]').value;
-        this.form = document.querySelector('#registration-form');
-        this.allFields = document.querySelectorAll('#registration-form .form-control');
-        this.insertValidationElements();
-        this.username = document.querySelector('#username-register');
-        this.username.previousValue = "";
-        this.email = document.querySelector('#email-register');
-        this.email.previousValue = "";
-        this.password = document.querySelector('#password-register');
-        this.password.previousValue = "";
-        this.username.isUnique = false;
-        this.email.isUnique = false;
-        this.events();
+  constructor() {
+    this.formSubmitHandler = this.formSubmitHandler.bind(this);
+    this._csrf = document.querySelector('[name="_csrf"]').value;
+    this.form = document.querySelector('#registration-form');
+    this.allFields = document.querySelectorAll(
+      '#registration-form .form-control'
+    );
+    this.insertValidationElements();
+    this.username = document.querySelector('#username-register');
+    this.username.previousValue = '';
+    this.email = document.querySelector('#email-register');
+    this.email.previousValue = '';
+    this.password = document.querySelector('#password-register');
+    this.password.previousValue = '';
+    this.username.isUnique = false;
+    this.email.isUnique = false;
+    this.events();
+  }
+
+  events() {
+    this.form.addEventListener('submit', this.formSubmitHandler);
+
+    this.username.addEventListener('keyup', () => {
+      this.isDifferent(this.username, this.usernameHandler);
+    });
+
+    this.email.addEventListener('keyup', () => {
+      this.isDifferent(this.email, this.emailHandler);
+    });
+
+    this.password.addEventListener('keyup', () => {
+      this.isDifferent(this.password, this.passwordHandler);
+    });
+
+    this.username.addEventListener('blur', () => {
+      this.isDifferent(this.username, this.usernameHandler);
+    });
+
+    this.email.addEventListener('blur', () => {
+      this.isDifferent(this.email, this.emailHandler);
+    });
+
+    this.password.addEventListener('blur', () => {
+      this.isDifferent(this.password, this.passwordHandler);
+    });
+  }
+
+  formSubmitHandler(e) {
+    e.preventDefault();
+    // Run each check manually
+    this.usernameImmediately();
+    this.usernameAfterDelay();
+    this.emailAfterDelay();
+    this.passwordImmediately();
+    this.passwordAfterDelay();
+
+    // Only submit form if username and email is unique and there is no errors in all fields
+    if (
+      this.username.isUnique &&
+      !this.username.errors &&
+      this.email.isUnique &&
+      !this.email.errors &&
+      !this.password.errors
+    ) {
+      this.form.submit();
+    }
+  }
+
+  isDifferent(el, handler) {
+    if (el.previousValue != el.value) {
+      handler.call(this); // Make 'this' still pointing to the registrationForm object
+    }
+    el.previousValue = el.value;
+  }
+
+  // Handle username key stroke: Respond with checking immedately and check after some delay
+  usernameHandler() {
+    this.username.errors = false; // Clean errors each time
+    this.usernameImmediately();
+    clearTimeout(this.username.timer);
+    this.username.timer = setTimeout(() => this.usernameAfterDelay(), 2000);
+  }
+
+  usernameImmediately() {
+    const usernameVal = this.username.value;
+    // User Regex to test if username contains non-alphanumeric characters
+    if (usernameVal !== '' && !/^([a-zA-Z0-9]+)$/.test(usernameVal)) {
+      this.showValidationError(
+        this.username,
+        'Username can only contain letters and numbers.'
+      );
     }
 
-    events() {
-        this.form.addEventListener('submit', this.formSubmitHandler);
-
-        this.username.addEventListener('keyup', () => {
-            this.isDifferent(this.username, this.usernameHandler);
-        });
-
-        this.email.addEventListener('keyup', () => {
-            this.isDifferent(this.email, this.emailHandler);
-        });
-
-        this.password.addEventListener('keyup', () => {
-            this.isDifferent(this.password, this.passwordHandler);
-        });
-
-        this.username.addEventListener('blur', () => {
-            this.isDifferent(this.username, this.usernameHandler);
-        });
-
-        this.email.addEventListener('blur', () => {
-            this.isDifferent(this.email, this.emailHandler);
-        });
-
-        this.password.addEventListener('blur', () => {
-            this.isDifferent(this.password, this.passwordHandler);
-        });
+    if (usernameVal.length > 30) {
+      this.showValidationError(
+        this.username,
+        "Username can't exceed 30 characters."
+      );
     }
 
-    formSubmitHandler(e) {
-        e.preventDefault();
-        // Run each check manually
-        this.usernameImmediately();
-        this.usernameAfterDelay();
-        this.emailAfterDelay();
-        this.passwordImmediately();
-        this.passwordAfterDelay();
+    if (!this.username.errors) {
+      this.hideValidationError(this.username);
+    }
+  }
 
-        // Only submit form if username and email is unique and there is no errors in all fields
-        if(this.username.isUnique && !this.username.errors && this.email.isUnique && !this.email.errors && !this.password.errors) {
-            this.form.submit();
-        }
+  usernameAfterDelay() {
+    const usernameVal = this.username.value;
+    if (usernameVal.length < 3) {
+      this.showValidationError(
+        this.username,
+        'Username must be at least 3 characters.'
+      );
     }
 
-    isDifferent(el, handler) {
-        if(el.previousValue != el.value) {
-            handler.call(this); // Make 'this' still pointing to the registrationForm object
-        }
-        el.previousValue = el.value;
-    }
-
-    // Handle username key stroke: Respond with checking immedately and check after some delay
-    usernameHandler() {
-        this.username.errors = false;   // Clean errors each time
-        this.usernameImmediately();
-        clearTimeout(this.username.timer);
-        this.username.timer = setTimeout(() => this.usernameAfterDelay(), 2000);
-    }
-
-    usernameImmediately() {
-        const usernameVal = this.username.value;
-        // User Regex to test if username contains non-alphanumeric characters
-        if(usernameVal !== "" && !/^([a-zA-Z0-9]+)$/.test(usernameVal)) {
-            this.showValidationError(this.username, "Username can only contain letters and numbers.");
-        }
-
-        if(usernameVal.length > 30) {
-            this.showValidationError(this.username, "Username can't exceed 30 characters.")
-        }
-
-        if(!this.username.errors) {
-            this.hideValidationError(this.username);
-        }
-    }
-
-    usernameAfterDelay() {
-        const usernameVal = this.username.value;
-        if(usernameVal.length < 3) {
-            this.showValidationError(this.username, "Username must be at least 3 characters.");
-        }
-
-        // Check if the username is already in the database
-        if(!this.username.errors) {
-            axios.post('/doesUsernameExist', { 
-                    _csrf: this._csrf,
-                    username: usernameVal 
-                })
-                .then((response) => {
-                    if(response.data) {
-                        this.showValidationError(this.username, "That username is already taken");
-                        this.username.isUnique = false;
-                    }
-                    else {
-                        this.username.isUnique = true;
-                    }
-                })
-                .catch((err) => {
-                    console.error(err);
-                });
-
-        }
-    }
-
-    emailHandler() {
-        this.email.errors = false;   // Clean errors each time
-        clearTimeout(this.email.timer);
-        this.email.timer = setTimeout(() => this.emailAfterDelay() , 2000);
-    }
-
-    emailAfterDelay() {
-        const emailVal = this.email.value;
-        if(!/^\S+@\S+$/.test(emailVal)) {
-            this.showValidationError(this.email, "You must provide a valid email address.");
-        }
-
-        if(!this.email.errors) {
-            axios.post('/doesEmailExist', { 
-                    _csrf: this._csrf,
-                    email: emailVal 
-                })
-                .then((response) => {
-                    if(response.data) {
-                        this.email.isUnique = false;
-                        this.showValidationError(this.email, "That email is already being used.");
-                    }
-                    else {
-                        this.email.isUnique = true;
-                        this.hideValidationError(this.email);
-                    }
-                })
-                .catch((err) => {
-                    console.error(err);
-                });
-        }
-    }
-
-    passwordHandler() {
-        this.password.errors = false;   // Clean errors each time
-        this.passwordImmediately();
-        clearTimeout(this.password.timer);
-        this.password.timer = setTimeout(() => this.passwordAfterDelay(), 2000);
-    }
-
-    passwordImmediately() {
-        if(this.password.value.length > 50) {
-            this.showValidationError(this.password, "Password cannot exceed 50 characters.");
-        }
-
-        if(!this.password.errors) {
-            this.hideValidationError(this.password);
-        }
-    }
-
-    passwordAfterDelay() {
-        if(this.password.value.length < 12) {
-            this.showValidationError(this.password, "Password must be at least 12 characters.");
-        }
-    }
-
-
-    hideValidationError(el) {
-        el.nextElementSibling.classList.remove("liveValidateMessage--visible");
-    }
-
-    showValidationError(el, message) {
-        // Insert the message to the <div> formed in insertValidationElements()
-        el.nextElementSibling.innerHTML = message;
-        el.nextElementSibling.classList.add("liveValidateMessage--visible");
-        el.errors = true;
-    }
-
-    insertValidationElements() {
-        this.allFields.forEach((el) => {
-            el.insertAdjacentHTML('afterend', '<div class="alert alert-danger small liveValidateMessage"></div>')
+    // Check if the username is already in the database
+    if (!this.username.errors) {
+      axios
+        .post('/doesUsernameExist', {
+          _csrf: this._csrf,
+          username: usernameVal,
+        })
+        .then((response) => {
+          if (response.data) {
+            this.showValidationError(
+              this.username,
+              'That username is already taken'
+            );
+            this.username.isUnique = false;
+          } else {
+            this.username.isUnique = true;
+          }
+        })
+        .catch((err) => {
+          console.error(err);
         });
     }
+  }
+
+  emailHandler() {
+    this.email.errors = false; // Clean errors each time
+    clearTimeout(this.email.timer);
+    this.email.timer = setTimeout(() => this.emailAfterDelay(), 2000);
+  }
+
+  emailAfterDelay() {
+    const emailVal = this.email.value;
+    if (!/^\S+@\S+$/.test(emailVal)) {
+      this.showValidationError(
+        this.email,
+        'You must provide a valid email address.'
+      );
+    }
+
+    if (!this.email.errors) {
+      axios
+        .post('/doesEmailExist', {
+          _csrf: this._csrf,
+          email: emailVal,
+        })
+        .then((response) => {
+          if (response.data) {
+            this.email.isUnique = false;
+            this.showValidationError(
+              this.email,
+              'That email is already being used.'
+            );
+          } else {
+            this.email.isUnique = true;
+            this.hideValidationError(this.email);
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }
+
+  passwordHandler() {
+    this.password.errors = false; // Clean errors each time
+    this.passwordImmediately();
+    clearTimeout(this.password.timer);
+    this.password.timer = setTimeout(() => this.passwordAfterDelay(), 2000);
+  }
+
+  passwordImmediately() {
+    if (this.password.value.length > 50) {
+      this.showValidationError(
+        this.password,
+        'Password cannot exceed 50 characters.'
+      );
+    }
+
+    if (!this.password.errors) {
+      this.hideValidationError(this.password);
+    }
+  }
+
+  passwordAfterDelay() {
+    if (this.password.value.length < 6) {
+      this.showValidationError(
+        this.password,
+        'Password must be at least 6 characters.'
+      );
+    }
+  }
+
+  hideValidationError(el) {
+    el.nextElementSibling.classList.remove('liveValidateMessage--visible');
+  }
+
+  showValidationError(el, message) {
+    // Insert the message to the <div> formed in insertValidationElements()
+    el.nextElementSibling.innerHTML = message;
+    el.nextElementSibling.classList.add('liveValidateMessage--visible');
+    el.errors = true;
+  }
+
+  insertValidationElements() {
+    this.allFields.forEach((el) => {
+      el.insertAdjacentHTML(
+        'afterend',
+        '<div class="alert alert-danger small liveValidateMessage"></div>'
+      );
+    });
+  }
 }
--- models/User.js
+++ models/User.js
@@ -5,200 +5,204 @@ const md5 = require('md5');
 const ObjectID = require('mongodb').ObjectID;
 
 class User {
-    // If getAvatar is true, it will automatically get avatar
-    constructor(data, getAvatar) {
-        this.data = data;
-        this.errors = [];
-        if(getAvatar === undefined) {
-            getAvatar = false;
-        }
-        if(getAvatar) {
+  // If getAvatar is true, it will automatically get avatar
+  constructor(data, getAvatar) {
+    this.data = data;
+    this.errors = [];
+    if (getAvatar === undefined) {
+      getAvatar = false;
+    }
+    if (getAvatar) {
+      this.getAvatar();
+    }
+  }
+
+  login() {
+    return new Promise((resolve, reject) => {
+      this.clearUp();
+      const { username, password } = this.data;
+      // .findOne() also returns a Promise
+      usersCollection
+        .findOne({ username })
+        .then((attemptUser) => {
+          if (
+            attemptUser &&
+            bcrypt.compareSync(password, attemptUser.password)
+          ) {
+            // Since login only enters username and password without email, we need to assign it back to this.data
+            this.data = attemptUser;
             this.getAvatar();
-        }
-    }
-
-    login() {
-        return new Promise((resolve, reject) => {
-            this.clearUp();
-            const { username, password } = this.data;
-            // .findOne() also returns a Promise
-            usersCollection.findOne({ username })
-                .then((attemptUser) => {
-                    if(attemptUser && bcrypt.compareSync(password, attemptUser.password)) {
-                        // Since login only enters username and password without email, we need to assign it back to this.data
-                        this.data = attemptUser;
-                        this.getAvatar();
-                        resolve("Congrat!");
-                    }
-                    else {
-                        reject("Invalid username / password");
-                    }
-                })
-                .catch((err) => {
-                    reject(err);
-                });
+            resolve('Congrat!');
+          } else {
+            reject('Invalid username / password');
+          }
+        })
+        .catch((err) => {
+          reject(err);
         });
+    });
+  }
+
+  clearUp() {
+    let { username, email, password } = this.data;
+
+    if (typeof username !== 'string') {
+      username = '';
+    }
+    if (typeof email !== 'string') {
+      email = '';
+    }
+    if (typeof password !== 'string') {
+      password = '';
     }
 
-    clearUp() {
-        let { username, email, password } = this.data;
-    
-        if(typeof(username) !== 'string') {
-            username = '';
+    // Get rid of extra properties entered by user, only keep username, email and password
+    this.data = {
+      username: username.trim().toLowerCase(),
+      email: email.trim().toLowerCase(),
+      password,
+    };
+  }
+
+  validate() {
+    return new Promise(async (resolve, reject) => {
+      const { username, email, password } = this.data;
+
+      if (username === '') {
+        this.errors.push('You must provide a username.');
+      }
+
+      if (username !== '' && !validator.isAlphanumeric(username)) {
+        this.errors.push('Username can only contain letters and numbers');
+      }
+
+      if (!validator.isEmail(email)) {
+        this.errors.push('You must provide an valid email.');
+      }
+
+      if (password === '') {
+        this.errors.push('You must provide a password.');
+      }
+
+      if (password.length > 0 && password.length < 6) {
+        this.errors.push('Password must be at least 6 characters');
+      }
+
+      if (password.length > 30) {
+        this.errors.push('Password cannot be over 30 characters');
+      }
+
+      if (username.length > 0 && username.length < 3) {
+        this.errors.push('Username must be at least 3 characters');
+      }
+
+      if (username.length > 30) {
+        this.errors.push('Username cannot be over 30 characters');
+      }
+
+      // Only check a valid username if it's already taken
+      if (
+        username.length > 2 &&
+        username.length < 31 &&
+        validator.isAlphanumeric(username)
+      ) {
+        const usernameExist = await usersCollection.findOne({ username });
+        if (usernameExist) {
+          this.errors.push('This username is taken!');
         }
-        if(typeof(email) !== 'string') {
-            email = '';
+      }
+
+      // Only check a valid email if it's already taken
+      if (validator.isEmail(email)) {
+        const emailExist = await usersCollection.findOne({ email });
+        if (emailExist) {
+          this.errors.push('This email is used!');
         }
-        if(typeof(password) !== 'string') {
-            password = '';
-        }
+      }
+      resolve();
+    });
+  }
 
-        // Get rid of extra properties entered by user, only keep username, email and password
-        this.data = {
-            username: username.trim().toLowerCase(),
-            email: email.trim().toLowerCase(),
-            password
-        };
-    }
+  register() {
+    return new Promise(async (resolve, reject) => {
+      this.clearUp();
+      await this.validate();
 
-    validate() {
-        return new Promise(async (resolve, reject) => {
-            const { username, email, password } = this.data;
+      if (this.errors.length === 0) {
+        // Hash the user password so in the database we store the hashed one
+        const salt = bcrypt.genSaltSync(10);
+        this.data.password = bcrypt.hashSync(this.data.password, salt);
+        await usersCollection.insertOne(this.data);
+        this.getAvatar();
+        resolve();
+      } else {
+        reject(this.errors);
+      }
+    });
+  }
 
-            if(username === "") {
-                this.errors.push("You must provide a username.");
-            }
-
-            if(username !== "" && !validator.isAlphanumeric(username)) {
-                this.errors.push("Username can only contain letters and numbers");
-            }
-
-            if(!validator.isEmail(email)) {
-                this.errors.push("You must provide an valid email.");
-            }
-
-            if(password === "") {
-                this.errors.push("You must provide a password.");
-            }
-
-            if(password.length > 0 && password.length < 12) {
-                this.errors.push("Password must be at least 12 characters");
-            }
-
-            if(password.length > 30) {
-                this.errors.push("Password cannot be over 30 characters");
-            }
-
-            if(username.length > 0 && username.length < 3) {
-                this.errors.push("Username must be at least 3 characters");
-            }
-
-            if(username.length > 30) {
-                this.errors.push("Username cannot be over 30 characters");
-            }
-
-            // Only check a valid username if it's already taken
-            if(username.length > 2 && username.length < 31 && validator.isAlphanumeric(username)) {
-                const usernameExist = await usersCollection.findOne({ username });
-                if(usernameExist) {
-                    this.errors.push("This username is taken!");
-                }
-            }
-
-            // Only check a valid email if it's already taken
-            if(validator.isEmail(email)) {
-                const emailExist = await usersCollection.findOne({ email });
-                if(emailExist) {
-                    this.errors.push("This email is used!");
-                }
-            }
-            resolve();
-        });
-    }
-
-    register() {
-        return new Promise(async (resolve, reject) => {
-            this.clearUp();
-            await this.validate();
-
-            if(this.errors.length === 0) {
-                // Hash the user password so in the database we store the hashed one
-                const salt = bcrypt.genSaltSync(10);
-                this.data.password = bcrypt.hashSync(this.data.password, salt);
-                await usersCollection.insertOne(this.data);
-                this.getAvatar();
-                resolve();
-            }
-            else {
-                reject(this.errors);
-            }
-        });
-    }
-
-    getAvatar() {
-        // The Gravatar uses md5 to hash the email address in the link to its avatar
-        this.avatar = `https://gravatar.com/avatar/${md5(this.data.email)}?s=128`;
-    }
+  getAvatar() {
+    // The Gravatar uses md5 to hash the email address in the link to its avatar
+    this.avatar = `https://gravatar.com/avatar/${md5(this.data.email)}?s=128`;
+  }
 }
 
 // We don't need to instantiate a new object to call this function:
-User.findByUsername = function(username) {
-    return new Promise((resolve, reject) => {
-        if(typeof(username) !== "string") {
-            reject();
-            return ;
+User.findByUsername = function (username) {
+  return new Promise((resolve, reject) => {
+    if (typeof username !== 'string') {
+      reject();
+      return;
+    }
+    usersCollection
+      .findOne({ username })
+      .then((userDoc) => {
+        if (userDoc) {
+          // Modify the returned userDoc so it only contains necessary data.
+          userDoc = new User(userDoc, true);
+          userDoc = {
+            _id: userDoc.data._id,
+            username: userDoc.data.username,
+            avatar: userDoc.avatar,
+          };
+          resolve(userDoc);
+        } else {
+          reject();
         }
-        usersCollection.findOne({ username })
-            .then((userDoc) => {
-                if(userDoc) {
-                    // Modify the returned userDoc so it only contains necessary data.
-                    userDoc = new User(userDoc, true);
-                    userDoc = {
-                        _id: userDoc.data._id,
-                        username: userDoc.data.username,
-                        avatar: userDoc.avatar
-                    };
-                    resolve(userDoc);
-                }
-                else {
-                    reject();
-                }
-            })
-            .catch(() => {
-                reject();
-            });
-    });
-}
+      })
+      .catch(() => {
+        reject();
+      });
+  });
+};
 
-User.doesEmailExist = function(email) {
-    return new Promise(async (resolve, reject) => {
-        if(typeof(email) !== "string") {
-            resolve(false);
-            return ;
-        }
-        const user = await usersCollection.findOne({ email });
-        if(user) {
-            // Email does exist
-            resolve(true);
-        }
-        else {
-            // Email does not exist
-            resolve(false);
-        }
-    });
-}
+User.doesEmailExist = function (email) {
+  return new Promise(async (resolve, reject) => {
+    if (typeof email !== 'string') {
+      resolve(false);
+      return;
+    }
+    const user = await usersCollection.findOne({ email });
+    if (user) {
+      // Email does exist
+      resolve(true);
+    } else {
+      // Email does not exist
+      resolve(false);
+    }
+  });
+};
 
 // Find Username by userId
-User.findUsernameById = function(id) {
-    return new Promise(async (resolve, reject) => {
-        const user = await usersCollection.findOne({ _id: new ObjectID(id)});
-        if(user) {
-            resolve(user.username);
-        }
-        else {
-            resolve("Anonymous");
-        }
-    });
-}
+User.findUsernameById = function (id) {
+  return new Promise(async (resolve, reject) => {
+    const user = await usersCollection.findOne({ _id: new ObjectID(id) });
+    if (user) {
+      resolve(user.username);
+    } else {
+      resolve('Anonymous');
+    }
+  });
+};
 
 module.exports = User;
